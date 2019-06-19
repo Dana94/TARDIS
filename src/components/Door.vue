f3753ff0947009136a77823d64d67b0f2a2f13d6
Dana Ottaviani <dana.ottaviani@gmail.com>
2019-06-19 10:24:01 -0400

    Add sign panel

--- src/components/Door.vue
+++ src/components/Door.vue
@@ -6,7 +6,13 @@
         <div class="window">
           <div v-for="i in 6" :key="i.id"></div>
         </div>
-        <div v-for="i in 7" :key="i.id" :class="{'pane': i % 2 != 0}"></div>
+        <div class="pane"></div>
+        <div class="sign">
+          <div>
+            <div v-for="i in 3" :key="i.id" :class="{'pane': i % 2 != 0}"></div>
+          </div>
+        </div>
+        <div v-for="i in 5" :key="i.id" :class="{'pane': i % 2 != 0}"></div>
       </div>
     </div>
     <div class="side">
@@ -15,7 +21,9 @@
         <div class="window">
           <div v-for="i in 6" :key="i.id"></div>
         </div>
-        <div v-for="i in 7" :key="i.id" :class="{'pane': i % 2 != 0}"></div>
+        <div class="pane"></div>
+        <div class="shield"></div>
+        <div v-for="i in 5" :key="i.id" :class="{'pane': i % 2 != 0}"></div>
       </div>
     </div>
   </div>
@@ -35,7 +43,7 @@ export default {};
   grid-template-columns: 1fr 1fr;
   .side {
     border: 1px solid black;
-    width: 60%;
+    width: 100%;
     margin: 0 auto;
     height: 100%;
     & > div {
@@ -43,6 +51,8 @@ export default {};
       grid-template: 15px 1fr 15px 1fr 15px 1fr 15px 1fr 15px / 1fr;
       border: 1px solid black;
       height: 100%;
+      width: 60%;
+      margin: 0 auto;
       .window {
         background-color: #ffffffb5;
         display: grid;
@@ -53,18 +63,26 @@ export default {};
           border: 1px solid white;
         }
       }
-      .pane {
+      &>.pane {
         height: 11px;
         // background-color: #4039c3;
         border-bottom: 1px solid;
         border-top: 1px solid;
       }
       & > div {
-        // background-color: #7066ac;
-        // margin: 15px;
-        // width: 55%;
         width: 100%;
         justify-self: center;
+        &.sign {
+          & > div {
+            width: 80%;
+            margin: 0 auto;
+            border: 1px solid;
+            height: 100%;
+            display: grid;
+            grid-template-rows: 10px 1fr 10px;
+            grid-template-columns: 1fr;
+          }
+        }
       }
     }
   }
