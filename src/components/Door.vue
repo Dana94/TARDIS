a849b057cc56575799b70c1e1db07a23ad36115d
Dana Ottaviani <dana.ottaviani@gmail.com>
2019-06-21 10:16:18 -0400

    Add call box knob

--- src/components/Door.vue
+++ src/components/Door.vue
@@ -10,7 +10,8 @@
           </div>
           <div class="pane"></div>
           <div class="sign">
-            <div>
+            <div class="knob"></div>
+            <div class="container">
               <div v-for="i in 3" :key="i.id" :class="{'pane': i % 2 != 0, 'poster': i % 2 == 0}"></div>
             </div>
           </div>
@@ -72,7 +73,7 @@ export default {};
         grid-template: 15px 1fr 15px 1fr 15px 1fr 15px 1fr 15px / 1fr;
         border: 1px solid black;
         height: 100%;
-        width: 60%;
+        width: 68%;
         margin: 0 auto;
         .window {
           background-color: #ffffffb5;
@@ -93,14 +94,25 @@ export default {};
           width: 100%;
           justify-self: center;
           &.sign {
-            & > div {
-              width: 76%;
+            position: relative;
+            .knob {
+              position: absolute;
+              background-color: #b9b9b9;
+              border: 1px solid black;
+              width: 4px;
+              height: 15px;
+              right: 3px;
+              top: 2rem;
+            }
+            .container {
+              width: 68%;
               margin: 0 auto;
               border: 1px solid;
               height: 100%;
               display: grid;
               grid-template-rows: 10px 1fr 10px;
               grid-template-columns: 1fr;
+
               .poster {
                 background-image: url("../assets/sign.jpg");
                 background-size: contain;
@@ -112,11 +124,11 @@ export default {};
       }
       .keyknob {
         position: absolute;
-        width: 10px;
-        height: 10px;
+        width: 8px;
+        height: 8px;
         background-color: #b9b9b9;
         border-radius: 50%;
-        left: 4px;
+        left: 2px;
         bottom: 12rem;
         display: inline-block;
       }
